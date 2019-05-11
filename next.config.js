e5903d3efe889a4fefe7bc3256fbef61d15ed6ac
Barnaby Keene <hello@southcla.ws>
2019-05-11 11:47:41 +0100

    added mdx support via plugin and a test page

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+const withPlugins = require('next-compose-plugins');
+
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
-module.exports = withCSS(
-  withImages({
-    env: {
-      GA_ID: 'UA-78828365-7'
-    }
-  })
-);
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/
+});
+
+module.exports = withPlugins([[withMDX], [withImages], [withCSS]], {
+  env: {
+    GA_ID: 'UA-78828365-7'
+  },
+  pageExtensions: ['js', 'jsx', 'mdx']
+});
